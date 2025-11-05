743938d86684387830b8992c350fa34a9237fe3a
Bashu-011 <bashu2609@gmail.com>
2025-11-05 15:48:01 +0300

    Task 0 | Basic IP Logging middleware

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,38 @@
+from .models import RequestLog
+from datetime import datetime
+
+class IPLoggingMiddleware:
+    """
+    middleware that logs the IP address/timestamp/request_path
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response  #next layer of middleware
+
+    def __call__(self, request):
+        #before accessing the ciew
+        ip = self.get_client_ip(request)
+        path = request.path
+
+        #log to db
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=path,
+            timestamp=datetime.now()
+        )
+
+        #move to next midleware in the chain
+        response = self.get_response(request)
+
+        return response
+
+    def get_client_ip(self, request):
+        """
+        get user IP from headers
+        """
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]  #first IP in the list
+        else:
+            ip = request.META.get('REMOTE_ADDR', '')
+        return ip
